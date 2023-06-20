f3141f44f14ff42f513c5413cc817702e1b48141
nafisimam <136448892+nafisimam@users.noreply.github.com>
2023-06-20 18:16:14 +0600

    Create loop-array.c

---
+++ loop-array.c
@@ -0,0 +1,16 @@
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int matrix[] = { 1, 4, 2, 3, 6, 8 };
+    int i;
+    
+    for(i=0; i < 6; i++){
+        printf("%d\n",matrix[i]);
+    }
+
+    return 0;
+}
