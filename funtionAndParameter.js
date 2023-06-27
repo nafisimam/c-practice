988e64478591c8dd4ccd1b2d9fa76abf62afc7ae
nafisimam <136448892+nafisimam@users.noreply.github.com>
2023-06-27 14:49:05 +0600

    Create funtionAndParameter.js

---
+++ funtionAndParameter.js
@@ -0,0 +1,41 @@
+// function factorial(n) {
+//   if (n <= 1) {
+//     return 1;
+//   } else {
+//     return n * factorial(n - 1);
+//   }
+// }
+
+
+
+
+function addTwoNumbers(x, y)
+{
+
+  // let num1 = x + 10
+  // let num2 = y + 20
+
+  return x + y
+  //console.log(sum)
+}
+
+function subtractTwoNumbers()
+{
+  let x = 2
+  let y = 1
+
+  let subtract
+  subtract = x - y
+
+  return subtract
+  //console.log(subtract)
+}
+
+let resultSum = addTwoNumbers(10,20)
+
+//let resultSubtract = subtractTwoNumbers(x,y)
+
+
+console.log(resultSum)
+//console.log(resultSubtract)
+
