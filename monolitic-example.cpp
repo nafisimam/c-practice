88f540c65fff467d51ea9c51a2d1f6d5f459fc20
nafisimam <136448892+nafisimam@users.noreply.github.com>
2023-07-14 21:04:50 +0600

    Update monolitic-example.cpp

--- monolitic-example.cpp
+++ monolitic-example.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
+int main()
+{
+    int lenth=0,breadth=0;
+    
+    printf("Enter lenth and breadth\n");
+    cin>>lenth>> breadth;
+    
+    cout<<lenth<<endl<<breadth<<endl;
+    
+    int area = lenth *breadth;
+    
+    int peri = 2*(lenth+breadth);
+    
+    cout<<area<<endl<<peri<<endl;
+
+    return 0;
+}
