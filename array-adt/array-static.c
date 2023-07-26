e589bb675d5f639d91c549f2fb8d98af3f3c3b5c
Imam Hossain Nafis <136448892+nafisimam@users.noreply.github.com>
2023-07-27 02:25:05 +0600

    Update array-static.c

--- array-adt/array-static.c
+++ array-adt/array-static.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct Array
 {
-    int A[20];
+    int A[10];
     int size;
     int length;
-};
+}; 
 
-void display(struct Array arr)
+void Display(struct Array arr)
 {
     int i;
     printf("All Array Elements:\n");
@@ -17,12 +16,34 @@ void display(struct Array arr)
         printf("%d\n", arr.A[i]);
     }  
 }
+void Append(struct Array *arr,int x) // structure pass by address is needed here to modify the values
+{
+    if(arr->length < arr->size) // checks if the array length is less then array size
+    {
+      arr->A[arr->length++]=x; // inserts element to the last position of array
+    }
+      
+}
+
+void Insert(struct Array *arr, int index, int x)
+{
+    int i;
+    if (index >= 0 && index <= arr->length)
+    {
+        for(i = arr->length; i > index; i--)
+        {
+            arr->A[i]=arr->A[i-1];
+        }
+        arr->A[index] = x;
+        arr->length++;
+    }
+}
 
 int main()
 {
     struct Array arr={{1,2,3,4,5},20,5};
-
-    display(arr);
-        
+    Append(&arr,10);
+    Insert(&arr, 100, 100);
+    Display(arr);
     return 0;
 }
